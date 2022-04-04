cff288eb2ad82e407018da17d9b9c33613006f13
valensarrr <101572268+valensarrr@users.noreply.github.com>
2022-04-04 11:19:16 -0300

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+
+    char nombre [32];
+    printf ("ingrese nombre");
+    scanf ("%s", nombre);
+    printf ("Hola %s\n", nombre);
+
+
+
+    return 0;
+}
